e2926ea5a0102a0775b98596b6159720e3376864
SYJoaa <wfsgsyj@gmail.com>
2021-08-12 11:21:56 +0800

    github commit 1

---
+++ src/main/java/com/company/Hello.java
@@ -0,0 +1,11 @@
+package com.company;
+
+/**
+ * SYJoaa
+ * 2021/8/12 0012
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
